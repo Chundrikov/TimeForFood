08a8f1db221ff17d7d88f5f5fc30f897d785f1ae
Мохник Наталья <nmohnik@neoflex.ru>
2021-07-26 16:59:05 +0300

    add service layer

--- Consumer/src/main/java/com/example/consumer/service/ConsumerService.java
+++ Consumer/src/main/java/com/example/consumer/service/ConsumerService.java
@@ -2,11 +2,14 @@ package com.example.consumer.service;
 
 import com.example.consumer.dto.ConsumerDto;
 import com.example.consumer.entity.Consumer;
+import com.example.consumer.exception.ConsumerNotFoundException;
+import com.example.consumer.exception.NoDataFoundException;
 import com.example.consumer.mapper.ConsumerMapper;
 import com.example.consumer.repository.ConsumerRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import javax.transaction.Transactional;
@@ -26,20 +29,39 @@ public class ConsumerService {
     }
 
     public ConsumerDto update(Long id, ConsumerDto consumerDto) {
-        Consumer created = mapper.toConsumer(consumerDto);
         Consumer updatedConsumer = repository.findById(id).orElseThrow(()
-                -> new ItemNotFoundException(id));
+                -> new ConsumerNotFoundException(id));
+        updatedConsumer.setName(consumerDto.getName());
+        updatedConsumer.setPhone(consumerDto.getPhone());
+        updatedConsumer.setAddress(consumerDto.getAddress());
+        updatedConsumer.setEmail(consumerDto.getEmail());
+        repository.save(updatedConsumer);
+
+        ConsumerDto newConsumerDto = mapper.toConsumerDto(updatedConsumer);
+        newConsumerDto.setModifiedAt(LocalDateTime.now());
+        return newConsumerDto;
     }
 
-    public ConsumerDto getConsumerById() {
-
+    public ConsumerDto getConsumerById(Long id) {
+         Consumer consumer = repository.findById(id).orElseThrow(()
+                -> new ConsumerNotFoundException(id));
+         return mapper.toConsumerDto(consumer);
     }
 
     public List<ConsumerDto> getAllConsumers() {
-
+        List<Consumer> allConsumers = repository.findAll();
+        if(allConsumers.isEmpty()) {
+            throw new NoDataFoundException();
+        }
+        return mapper.toListConsumersDto(allConsumers);
     }
 
-    public void deleteById() {
-
+    public void deleteById(Long id) {
+        try {
+            repository.deleteById(id);
+        }
+        catch (Exception e) {
+            new ConsumerNotFoundException(id);
+        }
     }
 }

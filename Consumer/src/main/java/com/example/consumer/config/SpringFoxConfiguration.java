c739b2c428489a93066e87b48943212f14a12d53
Мохник Наталья <nmohnik@neoflex.ru>
2021-09-06 13:10:40 +0300

    add swagger

---
+++ Consumer/src/main/java/com/example/consumer/config/SpringFoxConfiguration.java
@@ -0,0 +1,25 @@
+package com.example.consumer.config;
+
+import com.example.consumer.rest.ConsumerController;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+@Import(BeanValidatorPluginsConfiguration.class)
+public class SpringFoxConfiguration {
+
+    @Bean
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
